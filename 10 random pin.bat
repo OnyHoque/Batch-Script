2ad8afe57cf7332f3bce971c9d7b6013c9d57d20
Ony Hoque <onyhoque@gmail.com>
2020-01-16 22:13:37 +0600

    Add files via upload

---
+++ 10 random pin.bat
@@ -0,0 +1,26 @@
+echo off
+cls
+echo 10 random passwords:
+echo.
+echo %random%%random%
+echo.
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo. 
+echo %random%%random%
+echo.
+
+set /p input= Press enter to end.
